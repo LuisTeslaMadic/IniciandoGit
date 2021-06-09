9e3c5ed76a4667f72ee851ecc8e9f5c62cf1aae1
Luis <randi_junior144@hotmail.es>
2021-06-09 00:52:35 -0500

    Mi primer commit

---
+++ src/demo/Ejecucion.java
@@ -0,0 +1,11 @@
+package demo;
+
+public class Ejecucion {
+ 
+	public static void main(String args[]) {
+		int x = 0,y=1,z=2;
+		System.out.println(x);
+		System.out.println(y);
+		System.out.println(z);
+	}
+} 
